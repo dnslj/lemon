7f7796f1bd0046e3489276fe9deb1696d569fd08
liangjian <l_jian0015@126.com>
2019-12-30 22:50:22 +0800

    utils test

--- utils/utils/utils_test.go
+++ utils/utils/utils_test.go
@@ -2,9 +2,30 @@ package utils
 
 import (
 	"fmt"
+	"net/url"
 	"testing"
 )
 
+func TestFileIsExist(t *testing.T) {
+	fmt.Println(FileIsExist("./utils.go"))
+}
+
 func TestGetTimeStandar(t *testing.T) {
 	fmt.Println(GetTimeStandar())
+}
+
+func TestGetTimeUnix(t *testing.T) {
+	fmt.Println(GetTimeUnix())
+}
+
+func TestMD5(t *testing.T) {
+	fmt.Println(MD5("lemon"))
+}
+
+func TestCreateSign(t *testing.T) {
+	params := url.Values{
+		"method": {"get"},
+		"id":     {"1"},
+	}
+	fmt.Println(CreateSign(params))
 }
