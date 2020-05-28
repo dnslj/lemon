b3b86a86a2d40d3be6c3dd2b60a6906fdc7a87a2
liangjian <l_jian0015@126.com>
2020-05-28 11:12:04 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-SERVICE_NAME="lemon-web"
+SERVICE_NAME="frontend"
 SERVICE_HOME=$(echo $PWD)
 SERVICE_LOG_PATH='/tmp/'
 PROCESS_PATH="/cmd/bin/"
-LEMON_WEB_PATH="${SERVICE_HOME}${PROCESS_PATH}${SERVICE_NAME}"
+SERVICE_PATH="${SERVICE_HOME}${PROCESS_PATH}${SERVICE_NAME}"
 DATE_TIME=$(date "+%Y-%m-%d")
 
 # set msg
-info_msg="\033[;32m[INFO]\033[0m"
+info_msg="\033[;34m[INFO]\033[0m"
 warn_msg="\033[;33m[WARN]\033[0m"
 error_msg="\033[;31m[ERROR]\033[0m"
 success_msg="\033[;42m[SUCCESS]\033[0m"
@@ -23,7 +23,8 @@ buildServer() {
 
   BIN_FOLDER=${SERVICE_HOME}${PROCESS_PATH}
   if [ ! -d $BIN_FOLDER ]; then mkdir -p $BIN_FOLDER; fi
-  go build -o $LEMON_WEB_PATH ${SERVICE_HOME}/cmd/main.go
+  go build -o $SERVICE_PATH ${SERVICE_HOME}/cmd/main.go
+  chmod o+x $SERVICE_PATH
   echo "${success_msg} server built finished"
 }
 
@@ -48,7 +49,7 @@ function op_start() {
     echo "${info_msg} service: $SERVICE_NAME is running"
     exit 0
   fi
-  $LEMON_WEB_PATH >>${SERVICE_LOG_PATH}${SERVICE_NAME}-${DATE_TIME}.log 2>&1 &
+  $SERVICE_PATH >>${SERVICE_LOG_PATH}${SERVICE_NAME}-${DATE_TIME}.log 2>&1 &
   echo "\n${info_msg}service: $SERVICE_NAME is running, process details:"
   ps -ef | grep $SERVICE_NAME | grep -v grep
 }
